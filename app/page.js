721b842b856d8a07f2abbcf95c1510cd36f0f7de
ruibing <rbruibingnet@gmail.com>
2023-07-13 13:13:44 +0800

    added more stalls

--- app/page.js
+++ app/page.js
@@ -2,16 +2,33 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  const row1 = [];
-  for (let index = 0; index < 5; index++) {
-    row1.push(<div className={styles.stall}></div>);
-  }
+  const topRow = generateStalls(5);
+  const leftRow = generateStalls(11);
+  const rightRow = generateStalls(4);
 
   return (
     <main className={styles.main}>
       <div style={{"display" : "flex"}}>
-        {row1}
+        {topRow}
       </div>
+      <div style={{"display" : "flex"}}>
+        <div style={{"display" : "flex", "flexDirection" : "column", "marginRight" : "30%"}}>
+          {leftRow}
+        </div>
+        <div style={{"display" : "flex", "flexDirection" : "column"}}>
+          {rightRow}
+        </div>
+      </div>
+      
+      
     </main>
   )
+}
+
+function generateStalls(n) {
+  const stalls = [];
+  for (let i=0; i<n; i++) {
+    stalls.push(<div className={styles.stall}></div>);
+  }
+  return stalls;
 }
